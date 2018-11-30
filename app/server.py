9899579162c24774fd7545152e18075c8f2ab30e
Unknown <tushin.ka@phystech.edu>
2018-11-30 22:13:44 +0300

    last version

--- app/server.py
+++ app/server.py
@@ -4,7 +4,7 @@ import os
 from ML.Machine_Learning_part.malware_cheker import predict_file
 
 UPLOAD_FOLDER = '/home/kirill/Documents/Data Science/MIPT/information_security/data_upload/'
-MODEL_PATH = '/home/kirill/Documents/Data Science/MIPT/information_security/ML/Machine_Learning_part/best_classifier/'
+MODEL_PATH = '/home/kirill/Documents/Data Science/MIPT/information_security/app/ML/Machine_Learning_part/best_classifier/'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -14,15 +14,18 @@ Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    score = -1
     if request.method == 'POST':
-        file = request.files['file']
+        try:
+            file = request.files['file']
+        except:
+            return render_template('index.html')
         if file:
             filename = file.filename
 
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             score = predict_file(UPLOAD_FOLDER + filename, MODEL_PATH)
-            return render_template('index_score.html', filename=filename, score=score)
+
+            return render_template('{}.html'.format(score), filename=filename)
     return render_template('index.html')
 
 
